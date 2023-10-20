919246ff8317cdd834bd59f45f66946a105aed85
David Eduardo <davidesj97@gmail.com>
2023-10-19 21:48:12 -0600

    Inicio del proyecto

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+class Server {
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicación
+    this.routes();
+  }
+
+  middlewares() {
+    // Directorio publico
+    this.app.use( express.static('public') )
+  }
+
+  routes() {
+    this.app.get('/api', (req, res) => {
+      res.json('get API')
+    })
+
+    this.app.put('/api', (req, res) => {
+      res.json('put API')
+    })
+
+    this.app.post('/api', (req, res) => {
+      res.json('post API')
+    })
+
+    this.app.delete('/api', (req, res) => {
+      res.json('delete API')
+    })
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    })
+  }
+}
+
+module.exports = Server;
